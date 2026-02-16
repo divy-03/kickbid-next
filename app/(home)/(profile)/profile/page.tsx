5848e1b7028bd336c43b23c1bd92d1f08b1b270f
divy-03 <pathakdivy03@gmail.com>
2026-02-16 17:20:52 +0530

    added empty profile

--- app/(home)/(profile)/profile/page.tsx
+++ app/(home)/(profile)/profile/page.tsx
@@ -1,11 +1,40 @@
 import { getMyProfile } from "@/lib/actions/user-actions";
-import { Loader2 } from "lucide-react";
+import { PersonStanding } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty"
 
 export default async function ProfilePage() {
   const session = await getMyProfile();
   console.log("user", session);
   return (
-    <div>Profile
+    <div>
+      {session ? (
+        <div>
+          Profile
+        </div>
+      ) : (
+        <Empty>
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <PersonStanding />
+            </EmptyMedia>
+            <EmptyTitle>Profie Missing</EmptyTitle>
+            <EmptyDescription>
+              You haven&apos;t created your player profile yet</EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent className="flex-row justify-center gap-2">
+            <Button>Create Profile</Button>
+          </EmptyContent>
+        </Empty>
+      )
+      }
     </div>
   )
 }
