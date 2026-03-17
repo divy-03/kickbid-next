7fceb99cebb04b51f2ef7062f74ddb394d9d1c36
divy-03 <pathakdivy03@gmail.com>
2026-03-17 15:44:08 +0530

    added zod updateProfileSchema

--- lib/zod.ts
+++ lib/zod.ts
@@ -2,7 +2,16 @@ import { z } from "zod";
 
 export const addPlayerProfileSchema = z.object({
   position: z.string().min(2), // TODO: change it to enum
-  rating: z.number().int().positive()
+  rating: z.number().min(1).max(5)
 })
 
 export type AddPlayerProfileSchema = z.infer<typeof addPlayerProfileSchema>;
+
+export const updateProfileSchema = z.object({
+  name: z.string().min(2).optional(),
+  image: z.string().optional(),
+  rating: z.number().min(1).max(5).optional(),
+  position: z.string().optional() // TODO: change it to enum
+})
+
+export type UpdateProfileSchema = z.infer<typeof updateProfileSchema>;
