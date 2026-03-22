4d94326030943ddebc18fba389ba0443afa3d5db
divy-03 <pathakdivy03@gmail.com>
2026-03-22 13:47:13 +0530

    added next config for image from s3

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ui-avatars.com",
       },
+      {
+        protocol: "https",
+        hostname: "kickbid-user-images.s3.us-east-1.amazonaws.com"
+      }
     ],
     dangerouslyAllowSVG: true,
   },
